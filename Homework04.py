4929ae743e4ef8d86ded2940a226223d9551a5b4
Leonid.Bezutskiy <162831168+Toolrog@users.noreply.github.com>
2024-03-11 18:03:16 +0300

    Add files via upload

---
+++ Homework04.py
@@ -0,0 +1,17 @@
+#Tuple
+immutable_var = 1, 1.0, 'string', True
+print(type (immutable_var))
+print(immutable_var)
+ #Тuple(Кортеж) является неизменяемым типом данных, чтобы присвоить в нем новые значения,
+ #нужно вставить в него List(Список), и тогда можно список изменить внутри кортежа
+  #ПРИМЕР:
+immutable_var01 = [1, 2.0, 'string', True], 5
+immutable_var01 [0][2] = 5.6
+print (immutable_var01)
+#List
+Mutable_var = [1,2,5, True, 'string']
+Mutable_var.remove(True)
+Mutable_var.append(6.0)
+Mutable_var.extend(['apple'])
+print(type(Mutable_var))
+print(Mutable_var)
