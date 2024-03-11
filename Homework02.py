d0538699ec17609637c936273ce3382e4144c180
Toolrog <162831168+Toolrog@users.noreply.github.com>
2024-03-11 15:20:30 +0300

    Add files via upload

---
+++ Homework02.py
@@ -0,0 +1,16 @@
+#<Блок Имени>
+Id1 = 'Name: '
+Name = 'Leonid'
+print(Id1 + Name)
+#<Блок Возраста>
+Id2 = 'Age: '
+Age = 23
+print(Id2, Age)
+##<Блок Возраста + 1>
+Id3 = 'New age: '
+New_age = Age + 1
+print(Id3, New_age)
+##<Блок "Студента">
+Id4 = 'Is student: '
+is_student = True
+print(Id4, is_student)
