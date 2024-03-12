f368f507133e8b01274834df3f32634d107b5647
Leonid.Bezutskiy <162831168+Toolrog@users.noreply.github.com>
2024-03-12 14:00:33 +0300

    Add files via upload

---
+++ Homework05.py
@@ -0,0 +1,23 @@
+my_list = ['Orange', 'Lemon', 'apple', 2, True]
+print('list: ', my_list)
+        # Первый и последний элемент списка
+print('first: ', my_list[0])
+print('last: ', my_list[4])
+        # С 3 по 5 элементы списка
+print('sublist: ', my_list[2:5])
+        # Замена 3 элемента списка
+my_list[2] = ('coconut')
+print('Modified list', my_list)
+print(
+
+)
+        #___________
+
+my_dict = {}
+my_dict ['t-short'] = 'Футболка'
+my_dict ['dog'] = 'Собака'
+my_dict ['cat'] = 'Кошка'
+print('dictionary: ', my_dict)
+print('translation: ',my_dict['dog'])
+my_dict.setdefault('Orange', 'Апельсин')
+print('Modified dictionary', my_dict)
