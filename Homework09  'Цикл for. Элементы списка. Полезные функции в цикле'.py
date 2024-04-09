81bf298302d785772f1451b4f8f1f122925eb118
Leonid.Bezutskiy <162831168+Toolrog@users.noreply.github.com>
2024-04-09 15:18:53 +0300

    Update Homework09  'Цикл for. Элементы списка. Полезные функции в цикле'.py

--- Homework09  'Цикл for. Элементы списка. Полезные функции в цикле'.py
+++ Homework09  'Цикл for. Элементы списка. Полезные функции в цикле'.py
@@ -1,17 +1,6 @@
-# list_ = ['One', 'Two', 'Three']
-# list_2 = [2, 3, 4, 5, 1]
-# sum_ = 0
-# for i in range(len(list_2)):
-#     sum_ += list_2[i]
-#
-# print(sum_)
-
-# for i in range(1, 11):
-#     for j in range (1, 11):
-#         print(f'{i} x {j} = {i * j}')
-
 My_cars = ['BMW', 'Lada', 'Opel', 'Kia', 'Honda']
 Cars_count = 0
 for i in My_cars:
     Cars_count += 10
-    print('Я езжу на автомобиле марки', i, Cars_count)
+    print('Я езжу на автомобиле марки', i)
+    print('Катался на ней', Cars_count, 'раз')
